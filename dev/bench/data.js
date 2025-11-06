0c66c000a1eea1b632e087ae2d235eb33db6911f
github-action-benchmark <github@users.noreply.github.com>
2025-11-06 23:30:05 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for d68dcee041e41720a8b784a97e83d164be34649c

---
+++ dev/bench/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1762471805388,
+  "repoUrl": "https://github.com/sudhakara-ambati/lockless",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "su2k09.a@gmail.com",
+            "name": "Sudhakara Ambati",
+            "username": "sudhakara-ambati"
+          },
+          "committer": {
+            "email": "su2k09.a@gmail.com",
+            "name": "Sudhakara Ambati",
+            "username": "sudhakara-ambati"
+          },
+          "distinct": true,
+          "id": "d68dcee041e41720a8b784a97e83d164be34649c",
+          "message": "trigger benchmark workflow",
+          "timestamp": "2025-11-06T23:28:37Z",
+          "tree_id": "49a26fa20d233ca1c01aecb4490b8570918a7874",
+          "url": "https://github.com/sudhakara-ambati/lockless/commit/d68dcee041e41720a8b784a97e83d164be34649c"
+        },
+        "date": 1762471800526,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Enqueue",
+            "value": 9853480,
+            "unit": "ops/sec"
+          },
+          {
+            "name": "Dequeue",
+            "value": 11309000,
+            "unit": "ops/sec"
+          },
+          {
+            "name": "RoundTrip",
+            "value": 22199300,
+            "unit": "ops/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
